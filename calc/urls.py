8e2ba467d7e1693be6f01507041f9d034049d833
atobisch <tobischadrian@gmail.com>
2020-08-08 11:05:03 +0200

    calculator app, reduce the code in views with get und post

--- calc/urls.py
+++ calc/urls.py
@@ -6,9 +6,7 @@ app_name = 'calc'
 urlpatterns = [
     path('',views.home_view, name='home'),
     path('add_view/',views.add_view, name='add_view'),
-    path('add/',views.add, name='add'),
     path('mult_view/',views.mult_view, name='mult_view'),
-    path('mult/',views.mult, name='mult'),
     path('sub_view',views.sub_view, name='sub_view'),
-    path('sub',views.sub, name='sub'),
 ]
+
--- calc/views.py
+++ calc/views.py
@@ -1,31 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
 
 def home_view(request):
     return render(request,'calc/home.html')
 
 def add_view(request):
-    return render(request, 'calc/add_view.html')
-
-def add(request):
-    val1 = float(request.POST['num1'])
-    val2 = float(request.POST['num2'])
-    res = val1 + val2
-    return render(request, 'calc/result.html', {'result':res})
+    if request.method == 'POST':
+        val1 = float(request.POST['num1'])
+        val2 = float(request.POST['num2'])
+        res = val1 + val2
+        return render(request, 'calc/result.html', {'result': res})
+    elif request.method == 'GET':
+        return render(request, 'calc/add_view.html')
 
 def mult_view(request):
-    return render(request, 'calc/mult_view.html')
-
-def mult(request):
-    val1 = float(request.POST['num1'])
-    val2 = float(request.POST['num2'])
-    res = val1 * val2
-    return render(request, 'calc/result.html', {'result':res})
+    if request.method == 'POST':
+        val1 = float(request.POST['num1'])
+        val2 = float(request.POST['num2'])
+        res = val1 * val2
+        return render(request, 'calc/result.html', {'result': res})
+    elif request.method == 'GET':
+        return render(request, 'calc/mult_view.html')
 
 def sub_view(request):
-    return render(request, 'calc/sub_view.html')
-
-def sub(request):
-    val1 = float(request.POST['num1'])
-    val2 = float(request.POST['num2'])
-    res = val1 - val2
-    return render(request, 'calc/result.html', {'result':res})
+    if request.method == 'POST':
+        val1 = float(request.POST['num1'])
+        val2 = float(request.POST['num2'])
+        res = val1 - val2
+        return render(request, 'calc/result.html', {'result': res})
+    elif request.method == 'GET':
+        return render(request, 'calc/sub_view.html')
